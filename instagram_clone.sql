09b9098db41969dc521da0000bf1c5f01686f571
Muhammed Bahadir Kucuk <munisebahadir@gmail.com>
2020-06-23 00:59:27 +0200

    update

--- instagram_clone.sql
+++ instagram_clone.sql
@@ -10,3 +10,64 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT NOW()
 );
 
+--*********** PHOTOS ************
+
+CREATE TABLE photos (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    image_url VARCHAR(255) NOT NULL,
+    user_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+--******* WHO POSTED THE PHOTOS *********
+--SELECT photos.image_url, users.username FROM photos JOIN users ON photos.user_id = users.id;
+
+
+--*********** COMMENTS ************
+CREATE TABLE comments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    comment_text VARCHAR(255) NOT NULL,
+    user_id INTEGER NOT NULL,
+    photo_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(photo_id) REFERENCES photos(id)
+);
+
+--*********** LIKES ************
+CREATE TABLE likes (
+    user_id INTEGER NOT NULL,
+    photo_id INTEGER,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    PRIMARY KEY(user_id, photo_id)  -- to make likes for one time
+);
+
+--*********** FOLLOWS ************
+CREATE TABLE follows (
+    follower_id INTEGER NOT NULL,
+    followee_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(follower_id) REFERENCES users(id),
+    FOREIGN KEY(followee_id) REFERENCES users(id),
+    PRIMARY KEY(follower_id, followee_id) -- to make follows for one time
+);
+
+
+--*********** TAGS ************
+CREATE TABLE tags (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    tag_name VARCHAR(255) UNIQUE,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+--*********** PHOTO TAGS ************
+CREATE TABLE photo_tags (
+    photo_id INTEGER NOT NULL,
+    tag_id INTEGER NOT NULL,
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    FOREIGN KEY(tag_id) REFERENCES tags(id),
+    PRIMARY KEY(photo_id, tag_id)
+);
