c07d0aadca73fb0f2714d6989c672351c75ec621
Muhammed Bahadir Kucuk <munisebahadir@gmail.com>
2020-06-22 22:55:08 +0200

    Update instagram_clone.sql

--- instagram_clone.sql
+++ instagram_clone.sql
@@ -1,5 +1,12 @@
+DROP DATABASE instagram_clone;
 CREATE DATABASE instagram_clone;
 
 USE instagram_clone;
 
+
+CREATE TABLE users (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+);
 
